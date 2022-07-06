5ccd0e98ff3748678205f470b9ed6fdaef0102d0
Ana <amarcosestevez@gmail.com>
2022-07-06 12:51:38 +0200

    add function to change square background-color and function to stop interval

--- cuarto-reto/src/components/Square.jsx
+++ cuarto-reto/src/components/Square.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useRef,useState} from 'react';
 import '../styles/square.scss';
 const Square = () => {
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+let rgbColor = `rgb(${red},${green},${blue})`;
+const intervalRef = useRef();
+const [color, setColor] = useState(rgbColor);
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }  
+     function handleRandomColor() {
+const interval =
+setInterval(() => {
+       red = getRandomInt(0, 255);
+        green = getRandomInt(0, 255);
+        blue = getRandomInt(0, 255);
+        rgbColor = `rgb(${red},${green},${blue})`;
+        setColor(rgbColor)
+        return rgbColor; 
+      }, 500);
+      intervalRef.current = interval;
+    };
+ function handleStopChange(){
+    clearInterval(intervalRef.current);
+ }
     return (
-        <div className='container'>
-        <h1 className='container__title'>Square that changes his background-color</h1>
-            <i className="bi bi-square container__square"></i>
+        <div className='container' onMouseOver={handleRandomColor} onMouseOut={handleStopChange}>
+            <h1 className='container__title'>Square that changes his background-color</h1>
+            <div className="container__square" style={{ backgroundColor: color}} ></div>
         </div>
     );
 }
